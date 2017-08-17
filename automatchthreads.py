ffef9235c70760570bbd3220e75a22bf9c057f65
Andreas Höfter <andreashoefter@me.com>
2017-08-17 23:36:18 +0200

    file select

--- automatchthreads.py
+++ automatchthreads.py
@@ -1,15 +1,22 @@
 from modules.TournamentThread import TournamentThread
 from modules.Reddit import Reddit
 import os
-import tkinter as tk
-from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
 
-file_path = filedialog.askopenfilename()
+sortedFileList = sorted(os.listdir('tournaments'))
+for index, file in enumerate(sortedFileList):
+	print(str(index+1) + ' - ' + file)
+	if index == 19:
+		break
 
-tournamentThread = TournamentThread(file_path)
+fileindex = int(input('Which file? - '))
+
+if fileindex < 1 or fileindex > 20 or fileindex > len(sortedFileList):
+	print('Incorrect index')
+	quit()
+
+filepath = 'tournaments/' + sortedFileList[fileindex-1]
+tournamentThread = TournamentThread(filepath)
 
 tournamentThread.start()
 tournamentThread.join()
