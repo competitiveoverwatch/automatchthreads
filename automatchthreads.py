b8307c3d721268de89a8b41e280b824b995c43f8
Andreas Höfter <andreashoefter@me.com>
2017-08-17 23:55:06 +0200

    filename command line argument

--- automatchthreads.py
+++ automatchthreads.py
@@ -1,22 +1,12 @@
 from modules.TournamentThread import TournamentThread
 from modules.Reddit import Reddit
-import os
+import os, sys
 
-
-sortedFileList = sorted(os.listdir('tournaments'))
-for index, file in enumerate(sortedFileList):
-	print(str(index+1) + ' - ' + file)
-	if index == 19:
-		break
-
-fileindex = int(input('Which file? - '))
-
-if fileindex < 1 or fileindex > 20 or fileindex > len(sortedFileList):
-	print('Incorrect index')
+if len(sys.argv) < 2:
+	print('Give a file as command line argument')
 	quit()
-
-filepath = 'tournaments/' + sortedFileList[fileindex-1]
-tournamentThread = TournamentThread(filepath)
+	
+tournamentThread = TournamentThread(sys.argv[1])
 
 tournamentThread.start()
 tournamentThread.join()
